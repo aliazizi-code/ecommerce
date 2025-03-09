29a0d7e1309e0aa99744c1aefa0cb7e2ae05d913
aliazizi-code <devaliazizi@gmail.com>
2025-03-09 13:25:51 +0330

    Add initialization for product, comment, user favorites, and image models

---
+++ products/models/__init__.py
@@ -0,0 +1,20 @@
+# Product models
+from .product_models import (
+    CategoryProduct,
+    ColorProduct,
+    SizeProduct,
+    Product,
+    SpecificationsProduct,
+)
+
+# Comment models
+from .comment_models import (
+    CommentProduct,
+    VoteComment,
+)
+
+# User favorites
+from .user_favorites import FavoriteProduct
+
+# Image models
+from .image_models import ImagesProduct
