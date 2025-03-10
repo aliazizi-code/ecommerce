6b734cd7bc3f7001b8b95e141ad3948bc5ef433d
aliazizi-code <devaliazizi@gmail.com>
2025-03-10 16:50:00 +0330

    Optimize favorite and comment count updates in signals.py using F expressions for efficiency

--- products/signals.py
+++ products/signals.py
@@ -2,42 +2,67 @@ from django.db.models.signals import post_save, post_delete
 from django.dispatch import receiver
 from django.contrib.postgres.search import SearchVector
 from .models import FavoriteProduct, CommentProduct, Product
-from django.db.models import Avg
+from django.db.models import Avg, F
 
 
 # Favorites Logic
 @receiver(post_save, sender=FavoriteProduct)
-def increment_favorites_count(sender, instance, **kwargs):
-    instance.product.favorites_count += 1
-    instance.product.save()
+def increment_favorites_count(sender, instance, created, **kwargs):
+    if created:
+        Product.objects.filter(
+            id=instance.product.id
+        ).update(
+            favorites_count=F('favorites_count') + 1
+        )
 
 @receiver(post_delete, sender=FavoriteProduct)
 def decrement_favorites_count(sender, instance, **kwargs):
-    instance.product.favorites_count -= 1
-    instance.product.save()
+    Product.objects.filter(
+        id=instance.product.id
+    ).update(
+        favorites_count=F('favorites_count') - 1
+    )
 
 
 # Comments Logic
 @receiver(post_save, sender=CommentProduct)
 def increment_comment_count(sender, instance, created, **kwargs):
     if created:
-        avg_rating = CommentProduct.objects.filter(product=instance.product).aggregate(Avg('rating'))['rating__avg']
+        avg_rating = CommentProduct.objects.filter(
+            product=instance.product
+        ).aggregate(
+            Avg('rating')
+        )['rating__avg']
         
-        
-        instance.product.avg_rating = round(avg_rating, 1) if avg_rating is not None else 0
-        instance.product.comments_count += 1
-        instance.product.save()
+        Product.objects.filter(
+            id=instance.product.id
+        ).update(
+            avg_rating = round(avg_rating, 1) if avg_rating is not None else 0,
+            comments_count=F('comments_count') + 1
+        )
 
 @receiver(post_delete, sender=CommentProduct)
 def decrement_comment_count(sender, instance, **kwargs):
-    instance.product.comments_count -= 1
-    avg_rating = CommentProduct.objects.filter(product=instance.product).aggregate(Avg('rating'))['rating__avg']
-    instance.product.avg_rating = round(avg_rating, 1) if avg_rating is not None else 0
-    instance.product.save()
+    avg_rating = CommentProduct.objects.filter(
+        product=instance.product,
+        is_approved=True
+    ).aggregate(
+        Avg('rating')
+    )['rating__avg']
+    
+    Product.objects.filter(
+        id=instance.product.id
+    ).update(
+        avg_rating = round(avg_rating, 1) if avg_rating is not None else 0,
+        comments_count=F('comments_count') - 1
+    )
 
 
 # Product Logic
 @receiver(post_save, sender=Product)
 def update_search_vector(sender, instance, **kwargs):
-    instance.sv = SearchVector('name', 'tags', 'category__name', 'color__name', 'size__size')
-    instance.save()
+    Product.objects.filter(
+        id=instance.id
+    ).update(
+        sv=SearchVector('name', 'tags', 'category__name', 'color__name', 'size__size')
+    )
