8d8f14877ebc0ea310b4236e1016f27d38820cee
aliazizi-code <devaliazizi@gmail.com>
2025-03-12 18:58:12 +0330

    Update URL patterns to use integer IDs instead of slugs for product detail and comments

--- products/urls.py
+++ products/urls.py
@@ -3,13 +3,22 @@ from products import views
 
 
 urlpatterns = [
+    # Products
     path('', views.ProductsListViewSet.as_view({'get': 'list'}), name='product-list'),
-    path('detail/<slug:slug>/', views.ProductDetailView.as_view(), name='product-detail'),
-    path('category/', views.CategoriesListView.as_view(), name='category'),
-    path('like/', views.FavoriteProductView.as_view(), name='like'),
-    path('vote-comment/', views.VoteCommentView.as_view(), name='vote-comment'),
-    path('comments/<slug:slug>/', views.ProductCommentsListView.as_view(), name='comments-list'),
-    path('color-list/', views.ColorListView.as_view(), name='color-list'),
-    path('size-list/', views.SizeListView.as_view(), name='size-list'),
-    path('create-comment/', views.CreateCommentView.as_view(), name='create-comment'),
+    path('<int:id>/', views.ProductDetailView.as_view(), name='product-detail'),
+    
+    # Categories
+    path('categories/', views.CategoriesListView.as_view(), name='category-list'),
+
+    # Favorites
+    path('favorites/', views.FavoriteProductView.as_view(), name='favorite-product'),
+
+    # Comments
+    path('<int:id>/comments/', views.ProductCommentsListView.as_view(), name='comments-list'),
+    path('comments/vote/', views.VoteCommentView.as_view(), name='vote-comment'),
+    path('comments/create/', views.CreateCommentView.as_view(), name='create-comment'),
+
+    # Colors and Sizes
+    path('colors/', views.ColorListView.as_view(), name='color-list'),
+    path('sizes/', views.SizeListView.as_view(), name='size-list'),
 ]
