4e9610e736fbd81f79d0feaafba4219b27cbba24
aliazizi-code <devaliazizi@gmail.com>
2025-03-13 16:31:24 +0330

    Add email change request and verification serializers with validation

--- accounts/serializers/user_update_serializers.py
+++ accounts/serializers/user_update_serializers.py
@@ -1,6 +1,9 @@
 from rest_framework import serializers
 from accounts.models import UserProfile, User
 
+from utils import CacheManager
+from accounts.otp import verify_otp_change_email
+
 
 class UpdateUserProfileSerializer(serializers.ModelSerializer):
     class Meta:
@@ -9,7 +12,33 @@ class UpdateUserProfileSerializer(serializers.ModelSerializer):
         read_only_fields = ['user']
 
 
-class SetEmailSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = User
-        fields = ['email',]
+class ChangeEmailRequestSerializer(serializers.Serializer):
+    email = serializers.EmailField()
+
+    def validate_email(value):
+        if User.objects.filter(email=value).exists():
+            raise serializers.ValidationError("This email address already exists.")
+        return value
+
+
+class ChangeEmailVerifySerializer(serializers.Serializer):
+    email = serializers.EmailField()
+    otp = serializers.IntegerField(required=True)
+
+    def validate_email(self, value):
+        user = self.context['request'].user
+        cached_email = CacheManager.get_value(user.id, "new_email")
+
+        if cached_email is None:
+            raise serializers.ValidationError("No email found in the cache. Please request a new email verification.")
+
+        if cached_email != value:
+            raise serializers.ValidationError("The provided email does not match the cached email.")
+        
+        return value
+
+    def validate_otp(self, value):
+        user_id = self.context['user_id']
+
+        if not verify_otp_change_email(user_id, value):
+            raise serializers.ValidationError("Invalid OTP provided. Please try again.")
