86509aa66ddb2f98b0feaff0697ec2530d3e6267
aliazizi-code <devaliazizi@gmail.com>
2025-03-14 15:46:09 +0330

    Refactor password views to streamline OTP handling and simplify password reset logic

--- accounts/views/password_views.py
+++ accounts/views/password_views.py
@@ -3,11 +3,14 @@ from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework import permissions
-from accounts.serializers.password_serializers import *
-from accounts.otp import *
 from accounts.models import User
 from accounts.tasks import send_otp_to_email_tasks, send_otp_to_phone_tasks
-
+from accounts.otp import generate_otp_pass
+from accounts.serializers import (
+    ForgotPasswordRequestSerializer,
+    ForgotPasswordVerifySerializer,
+    ChangePasswordSerializer
+)
 
 
 class ForgotPasswordRequestView(APIView):
@@ -23,21 +26,17 @@ class ForgotPasswordRequestView(APIView):
 
             if number:
                 user = get_object_or_404(User, number=number)
-                if user.number == data['number']:
-                    otp = generate_otp_pass(user.id)
-                    send_otp_to_phone_tasks.delay(otp)
-
-                    return Response({"detail": "OTP sent successfully."}, status=status.HTTP_200_OK)
-                return Response({"detail": "New number must be different from the current number."}, status=status.HTTP_400_BAD_REQUEST)
+                otp = generate_otp_pass(user.id)
+                send_otp_to_phone_tasks.delay(otp)
+                return Response({"detail": "OTP sent successfully."}, status=status.HTTP_200_OK)
+                
             
             if email:
                 user = get_object_or_404(User, email=email)
-                if user.email != data['email']:
-                    otp = generate_otp_pass(user.id)
-                    send_otp_to_email_tasks.delay(otp)
-
-                    return Response({"detail": "OTP sent successfully."}, status=status.HTTP_200_OK)
-                return Response({"detail": "New email must be different from the current email."}, status=status.HTTP_400_BAD_REQUEST)
+                otp = generate_otp_pass(user.id)
+                send_otp_to_email_tasks.delay(otp)
+                return Response({"detail": "OTP sent successfully."}, status=status.HTTP_200_OK)
+                
         
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
@@ -52,28 +51,19 @@ class ForgotPasswordVerifyView(APIView):
             data = serializer.validated_data
             number = data.get('number')
             email = data.get('email')
-            otp = data['otp']
             new_password = data['password']
 
             if number:
                 user = get_object_or_404(User, number=number)
-                if user.number == data['number']:
-                    if verify_otp_pass(user.id, otp):
-                        user.set_password(new_password)
-                        user.save()
-                        return Response({"detail": "Password reset successfully."}, status=status.HTTP_200_OK)
-                    return Response({"detail": "Invalid OTP."}, status=status.HTTP_400_BAD_REQUEST)
-                return Response({"detail": "New number must be different from the current number."}, status=status.HTTP_400_BAD_REQUEST)
+                user.set_password(new_password)
+                user.save()
+                return Response({"detail": "Password reset successfully."}, status=status.HTTP_200_OK)
             
             if email:
                 user = get_object_or_404(User, email=email)
-                if user.email != data['email']:
-                    if verify_otp_pass(user.id, otp):
-                        user.set_password(new_password)
-                        user.save()
-                        return Response({"detail": "Password reset successfully."}, status=status.HTTP_200_OK)
-                    return Response({"detail": "Invalid OTP."}, status=status.HTTP_400_BAD_REQUEST)
-                return Response({"detail": "New email must be different from the current email."}, status=status.HTTP_400_BAD_REQUEST)
+                user.set_password(new_password)
+                user.save()
+                return Response({"detail": "Password reset successfully."}, status=status.HTTP_200_OK) 
         
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
@@ -88,11 +78,9 @@ class ChangePasswordView(APIView):
 
         if serializer.is_valid():
             data = serializer.validated_data
-            old_password = data['old_password']
             new_password = data['password']
 
-            if user.check_password(old_password):
-                user.set_password(new_password)
-                user.save()
-                return Response({"detail": "Password changed successfully."}, status=status.HTTP_200_OK)
-            return Response({"detail": "Invalid password."}, status=status.HTTP_400_BAD_REQUEST)
+            user.set_password(new_password)
+            user.save()
+            return Response({"detail": "Password changed successfully."}, status=status.HTTP_200_OK)
+            
