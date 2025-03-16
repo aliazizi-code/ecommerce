483d2a5e39147fdcf85df23599f9aacdfc1ed0f3
aliazizi-code <devaliazizi@gmail.com>
2025-03-16 15:08:46 +0330

    Refactor PasswordField to use RegexValidator for password validation

--- accounts/serializers/password_serializers.py
+++ accounts/serializers/password_serializers.py
@@ -1,35 +1,21 @@
 from rest_framework import serializers
 from .auth_serializers import PhoneNumberField
-import re
+from django.core.validators import RegexValidator
 from accounts.models import User
 from django.shortcuts import get_object_or_404
 from accounts.otp import verify_otp_pass
 
 
 class PasswordField(serializers.CharField):
-    def __init__(self, *args, **kwargs):
-        super(PasswordField, self).__init__(required=True, write_only=True, min_length=8, *args, **kwargs)
+    default_validators = [
+        RegexValidator(
+            regex=r'^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.*[!@#$%^&*()_+{}":;\']).{8,}$',
+            message="Password must be at least 8 characters long and include at least one uppercase letter, one lowercase letter, one digit, and one special character."
+        )
+    ]
 
-        def validate(self, value):
-            # Validate password length
-            if len(value) < 8:
-                raise serializers.ValidationError("Password must be at least 8 characters long.")
-            
-            # Validate password contains a uppercase letter
-            if not re.search(r'[A-Z]', value):
-                raise serializers.ValidationError("Password must contain at least one uppercase letter.")
-            
-            # Validate password contains a lowercase letter
-            if not re.search(r'[a-z]', value):
-                raise serializers.ValidationError("Password must contain at least one lowercase letter.")
-            
-            # Validate password contains a number
-            if not re.search(r'[0-9]', value):
-                raise serializers.ValidationError("Password must contain at least one number.")
-            
-            # Validate password contains a special character
-            if not re.search(r'[!@#$%^&*()_+{}":;\']', value):
-                raise serializers.ValidationError("Password must contain at least one special character.")
+    def __init__(self, *args, **kwargs):
+        super(PasswordField, self).__init__(required=True, write_only=True, *args, **kwargs)
 
 
 class PhoneEmailBaseSerializer(serializers.Serializer):
