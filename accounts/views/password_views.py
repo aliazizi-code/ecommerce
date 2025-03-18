968af77a54fd4389ee2e58c012589b4bf9aabd1e
aliazizi-code <devaliazizi@gmail.com>
2025-03-18 21:02:52 +0330

    Refactor password reset views to enhance OTP handling and streamline response generation

--- accounts/views/password_views.py
+++ accounts/views/password_views.py
@@ -5,7 +5,8 @@ from rest_framework.views import APIView
 from rest_framework import permissions
 from accounts.models import User
 from accounts.tasks import send_otp_to_email_tasks, send_otp_to_phone_tasks
-from accounts.otp import generate_otp_pass
+from accounts.otp import generate_otp_pass, delete_otp_pass, OTP_TIMEOUT
+from utils import CacheManager
 from accounts.serializers import (
     ForgotPasswordRequestSerializer,
     ForgotPasswordVerifySerializer,
@@ -28,17 +29,28 @@ class ForgotPasswordRequestView(APIView):
                 user = get_object_or_404(User, number=number)
                 otp = generate_otp_pass(user.id)
                 send_otp_to_phone_tasks.delay(otp)
-                return Response({"detail": "OTP sent successfully."}, status=status.HTTP_200_OK)
+                CacheManager.set_new_value(user.id, 'number', 'type_forget_pass', OTP_TIMEOUT)
+                return self._generate_response(user, 'number')
                 
             
             if email:
                 user = get_object_or_404(User, email=email)
                 otp = generate_otp_pass(user.id)
                 send_otp_to_email_tasks.delay(otp)
-                return Response({"detail": "OTP sent successfully."}, status=status.HTTP_200_OK)
-                
+                CacheManager.set_new_value(user.id, 'email', 'type_forget_pass', OTP_TIMEOUT)
+                return self._generate_response(user, 'email')
+        
         
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    def _generate_response(self, user, otp_type):
+            return Response(data={
+                "detail": "OTP sent successfully.",
+                "user_id": user.id,
+                "otp_type": otp_type
+        }, status=status.HTTP_200_OK)
+    
+    
 
 
 class ForgotPasswordVerifyView(APIView):
@@ -49,25 +61,36 @@ class ForgotPasswordVerifyView(APIView):
 
         if serializer.is_valid():
             data = serializer.validated_data
-            number = data.get('number')
-            email = data.get('email')
             new_password = data['password']
+            user_id = data.get('user_id')
+            email = data.get('email')
+            number = data.get('number')
+            otp_type = CacheManager.get_value(user_id, 'type_forget_pass')
 
-            if number:
-                user = get_object_or_404(User, number=number)
-                user.set_password(new_password)
-                user.is_active = True
-                user.save()
-                return Response({"detail": "Password reset successfully."}, status=status.HTTP_200_OK)
+            if otp_type == 'email' and email:
+                return self._reset_password(user_id, new_password, email, 'email')
+
+            if otp_type == 'number' and number:
+                return self._reset_password(user_id, new_password, number, 'number')
             
-            if email:
-                user = get_object_or_404(User, email=email)
-                user.set_password(new_password)
-                user.is_active = True
-                user.save()
-                return Response({"detail": "Password reset successfully."}, status=status.HTTP_200_OK) 
+            
         
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    def _generate_response(self):
+            return Response(data={"detail": "Password reset successfully."}, status=status.HTTP_200_OK)
+    
+    def _reset_password(self, user_id, new_password, identifier, identifier_type):
+        if identifier_type == 'email':
+            user = get_object_or_404(User, email=identifier, id=user_id)
+        else:
+            user = get_object_or_404(User, number=identifier, id=user_id)
+        
+        user.set_password(new_password)
+        user.save()
+        CacheManager.delete_value(user.id, 'type_forget_pass')
+        delete_otp_pass(user.id)
+        return self._generate_response()
 
 
 class ChangePasswordView(APIView):
